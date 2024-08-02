0a1fe1c7022ce227c7d58f7c4e79ea4104be23f1
Antonio Andres Andreu Barreiro <aaandreu@ustspain.es>
2024-08-02 14:31:40 +0200

    first commit

---
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { NgClass, NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-side-bar',
+  standalone: true,
+  imports: [NgClass, NgFor],
+  templateUrl: './side-bar.component.html',
+  styleUrl: './side-bar.component.css'
+})
+export class SideBarComponent {
+  mainMenu: {
+    defaultOptions: Array<any>, accessLink: Array<any>
+  } = { defaultOptions: [], accessLink: [] }
+
+  customOptions: Array<any> = []
+
+  ngOnInit(): void {
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Home',
+        icon: 'uil uil-estate',
+        router: ['/', 'auth']
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil uil-search',
+        router: ['/', 'history']
+      },
+      {
+        name: 'Tu biblioteca',
+        icon: 'uil uil-chart',
+        router: ['/', 'favorites'],
+        query: { hola: 'mundo' }
+      }
+    ]
+
+    this.mainMenu.accessLink = [
+      {
+        name: 'Crear lista',
+        icon: 'uil-plus-square'
+      },
+      {
+        name: 'Canciones que te gustan',
+        icon: 'uil-heart-medical'
+      }
+    ]
+
+    this.customOptions = [
+      {
+        name: 'Mi lista º1',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º2',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º3',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º4',
+        router: ['/']
+      }
+    ]
+
+  }
+
+}
+
+
